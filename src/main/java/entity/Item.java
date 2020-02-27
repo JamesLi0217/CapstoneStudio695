e0b19f26205e24511cb4b463503517d2ee7fcdbd
JamesLi0217 <pli14@stevens.edu>
2020-02-27 00:04:27 -0500

    Purify the data from GitHub Job Api, and store in Class Item

---
+++ src/main/java/entity/Item.java
@@ -0,0 +1,64 @@
+package entity;
+
+import java.util.Set;
+
+public class Item {
+	private String itemId;
+	private String name;
+	private String address;
+	private Set<String> keywords;
+	private String imageUrl;
+	private String url;
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Set<String> getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(Set<String> keywords) {
+		this.keywords = keywords;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	// constructors with so many fields
+	
+
+}
