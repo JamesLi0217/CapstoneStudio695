ce747e508bc15ea26a28044a2d4f6dc9e3ad212d
JamesLi0217 <pli14@stevens.edu>
2020-03-03 10:41:11 -0500

    ItemHistory servlet to handle get post delete favorite item

--- src/main/java/rpc/SearchItem.java
+++ src/main/java/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
+import db.MySQLConnection;
 import entity.Item;
 import externalAPI.GitHubClient;
 
@@ -32,18 +35,25 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 
 		GitHubClient client = new GitHubClient();
-//		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
 		List<Item> items = client.search(lat, lon, null);
+
+		MySQLConnection connection = new MySQLConnection();
+		Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+		connection.close();
+
 		JSONArray array = new JSONArray();
 		for (Item item : items) {
-			array.put(item.toJSONObject());
+			JSONObject obj = item.toJSONObject();
+			obj.put("favorite", favoritedItemIds.contains(item.getItemId()));
+			array.put(obj);
 		}
 		RpcHelper.writeJsonArray(response, array);
+
 	}
 
 	/**
