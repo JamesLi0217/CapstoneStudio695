8cd66db2d78a970d66b50c8a202a5d76ed93c5a1
JamesLi0217 <pli14@stevens.edu>
2020-03-04 20:22:39 -0500

    recommend algorithm recommend rpc

--- src/main/java/rpc/RecommendItem.java
+++ src/main/java/rpc/RecommendItem.java
@@ -1,10 +1,18 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+
+import entity.Item;
+import recommendation.Recommendation;
 
 /**
  * Servlet implementation class RecommendItem
@@ -25,15 +33,22 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.setStatus(403);
+			return;
+		}
+		String userId = request.getParameter("user_id");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
 
+		Recommendation recommendation = new Recommendation();
+		List<Item> items = recommendation.recommendItems(userId, lat, lon);
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		RpcHelper.writeJsonArray(response, array);
+	}
 }
